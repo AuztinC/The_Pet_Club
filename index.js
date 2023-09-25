09a5690315019beecb4192796c1ba3b7ea156112
Austin Cripe <aaustinc@live.com>
2023-09-25 16:44:01 -0400

    front end working

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_pet_club_db');
 const express = require('express');
 const app = express();
 const path = require('path');
+
+app.use(express.json())
 
 const homePage = path.join(__dirname, 'index.html');
 app.get('/', (req, res)=> res.sendFile(homePage));
@@ -16,12 +18,64 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
+app.get('/api/owners', async(req,res,next)=>{
+  try {
+    const response = await client.query(`SELECT * FROM owners ORDER BY id;`)
+    res.send(response.rows)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.get('/api/pets', async(req,res,next)=>{
+  try {
+    const response = await client.query(`SELECT * FROM pets ORDER BY id;`)
+    res.send(response.rows)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.put('/api/pets/:id', async(req,res,next)=>{
+  try {
+    const SQL = `
+    UPDATE pets
+    SET name = $1, owner_id = $2
+    WHERE id = $3
+    RETURNING *
+    `;
+    const response = await client.query(SQL, [req.body.name, req.body.owner_id, req.params.id])
+    res.send(response.rows[0])
+  } catch (error) {
+    next(error)
+  }
+})
+
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
+    DROP TABLE IF EXISTS owners;
+    DROP TABLE IF EXISTS pets;
+    CREATE TABLE owners(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20)
+    );
+    CREATE TABLE pets(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      owner_id INT
+    );
+    INSERT INTO owners (name) VALUES ('janyce');
+    INSERT INTO owners (name) VALUES ('muarice');
+    INSERT INTO owners (name) VALUES ('clyde');
+
+    INSERT INTO pets (name, owner_id) VALUES ('lucky', 2);
+    INSERT INTO pets (name, owner_id) VALUES ('chuck', 2);
+    INSERT INTO pets (name) VALUES ('precious');
+    INSERT INTO pets (name) VALUES ('hank');
   `;
+  await client.query(SQL)
   console.log('create your tables and seed data');
 
   const port = process.env.PORT || 3000;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const App = ()=> {
-  return (
-    <h1>FullStack Template</h1>
-  );
+  const [owners, setOwners] = useState([])
+  const [pets, setPets] = useState([])
+
+  useEffect(()=>{
+    const fetchOwners = async()=>{
+      const response = await axios.get('/api/owners')
+      setOwners(response.data)
+    }
+    const fetchPets = async()=>{
+      const response = await axios.get('/api/pets')
+      setPets(response.data)
+    }
+    fetchOwners()
+    fetchPets()
+  }, [])
+
+  async function addOwner(pet, owner){
+    pet = {...pet, owner_id: owner.id}
+    const response = await axios.put(`api/pets/${pet.id}`, pet)
+    setPets(pets.map(pet=>pet.id === response.data.id ? response.data : pet))
+  }
+  async function removeOwner(pet){
+    pet = {...pet, owner_id: null}
+    const response = await axios.put(`api/pets/${pet.id}`, pet)
+    setPets(pets.map(pet=>pet.id === response.data.id ? response.data : pet))
+  }
+
+  return (<>
+    <h1> AC PET CLUB </h1>
+    <div className='main'>
+      <div>
+        <h2>Owners</h2>
+        <ul>
+          {
+          owners.map(owner=>{
+            const myPets = pets.filter(pet=>pet.owner_id === owner.id)
+            return (
+              <li key={owner.id}>
+                {owner.name} ({ myPets.length })
+              </li>
+            )
+          })
+          }
+        </ul>
+      </div>
+      <div>
+        <h2>Pets</h2>
+        <ul>
+          {
+          pets.map(pet=>{
+            return (
+              <li key={pet.id}>
+                {pet.name}
+                <ul>
+                {
+                  owners.map(owner=>{
+                    return (
+                      <li key={owner.id}>
+                        { owner.name }
+                        {
+                          pet.owner_id === owner.id ? <button onClick={()=>removeOwner(pet)}>Remove</button> : <button onClick={()=>addOwner(pet, owner)}>Add</button>
+                        }
+                      </li>
+                    )
+                  })
+                }
+                </ul>
+              </li>
+            )
+          })
+          }
+        </ul>
+      </div>
+    </div>
+
+  </>);
 };
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
